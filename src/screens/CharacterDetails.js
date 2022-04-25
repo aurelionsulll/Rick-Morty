1ea4035a57f1da7823bf691b7626905530b4c059
mehdigeb <mehdigeb@gmail.com>
2022-04-25 00:14:48 +0000

    refresh to get new data

--- src/screens/CharacterDetails.js
+++ src/screens/CharacterDetails.js
@@ -1,15 +1,43 @@
-import { Text, View, ScrollView, SafeAreaView, TextInput } from 'react-native';
+import { Text, View, ScrollView, SafeAreaView, TextInput, Image } from 'react-native';
 import tw from 'twrnc';
+import { Icon } from 'react-native-elements';
 
 function CharacterDetails({
 	route: {
 		params: { character },
 	},
+	navigation: { goBack },
 }) {
 	return (
-		<SafeAreaView style={tw`bg-emerald-500 h-full `}>
-			<Text style={tw`mt-10 text-center text-gray-800 text-3xl font-bold `}>{character.name}</Text>
-		</SafeAreaView>
+		<ScrollView style={tw`bg-emerald-100 h-full pb-96`}>
+			<View style={tw`h-full`}>
+				<View style={tw`min-h-1/2 `}>
+					<Image style={tw`h-full w-full rounded-b-3xl`} source={{ uri: character.image }} />
+					<View style={tw`flex-row absolute items-center justify-center bottom-5 right-5 bg-white px-4 py-1 rounded-full`}>
+						<View
+							style={tw`${
+								character.status == 'unknown' ? 'bg-gray-500' : character.status == 'Alive' ? 'bg-green-500' : 'bg-red-500'
+							} w-4 h-4 rounded-full`}
+						/>
+						<Text style={tw`text-gray-800 font-light text-lg ml-3 shadow-2xl`}>{character.status}</Text>
+					</View>
+					<View style={tw`absolute top-12 left-4 bg-white rounded-full p-0.5 shadow-2xl `}>
+						<Icon name='chevron-left' color='#000444' size={43} onPress={() => goBack()} />
+					</View>
+				</View>
+				<Text style={tw` text-center mt-2 text-gray-800 text-3xl font-bold `}>{character.name}</Text>
+				<View style={tw`mx-8 mt-6 `}>
+					<Text style={tw`text-emerald-800 text-2xl font-bold `}>Species : </Text>
+					<Text style={tw`text-gray-800 text-lg font-bold mt-1`}>{character.species}</Text>
+					<Text style={tw`text-emerald-800 text-2xl font-bold mt-4`}>Gender : </Text>
+					<Text style={tw`text-gray-800 text-lg font-bold mt-1`}>{character.gender}</Text>
+					<Text style={tw`text-emerald-800 text-2xl font-bold mt-4`}>Origin : </Text>
+					<Text style={tw`text-gray-800 text-lg font-bold mt-1`}>{character.origin.name}</Text>
+					<Text style={tw`text-emerald-800 text-2xl font-bold mt-4`}>Last known location : </Text>
+					<Text style={tw`text-gray-800 text-lg font-bold mt-1`}>{character.location.name}</Text>
+				</View>
+			</View>
+		</ScrollView>
 	);
 }
 
--- src/screens/Home.js
+++ src/screens/Home.js
@@ -1,4 +1,4 @@
-import { Text, View, ScrollView, SafeAreaView, TextInput, TouchableOpacity } from 'react-native';
+import { Text, View, ScrollView, SafeAreaView, TextInput, TouchableOpacity, RefreshControl } from 'react-native';
 import tw from 'twrnc';
 import axios from 'axios';
 import React, { useEffect, useState } from 'react';
@@ -11,16 +11,40 @@ export default function Home({ navigation }) {
 	const [characters, setCharacters] = useState([]);
 	const [locations, setLocations] = useState([]);
 	const [episodes, setEpisodes] = useState([]);
+	const [filteredData, setFilteredData] = useState([]);
+	const [search, setSearch] = useState('');
+	const [refreshing, setRefreshing] = React.useState(false);
+	const [random, setRandom] = useState(1);
+	const wait = (timeout) => {
+		return new Promise((resolve) => setTimeout(resolve, timeout));
+	};
+	const onRefresh = React.useCallback(() => {
+		var rand = Math.floor(Math.random() * (1 - 42 + 1)) + 42;
+		setRefreshing(true);
+		setRandom(rand);
+		console.log(random);
+		wait(2000).then(() => setRefreshing(false));
+	}, []);
 
 	useEffect(() => {
-		axios.get('https://rickandmortyapi.com/api/character')
+		axios.get(`https://rickandmortyapi.com/api/character/?page=${random}`)
 			.then(function (response) {
-				setCharacters(response.data.results);
+				var data = [];
+				if (search == '') {
+					data = response.data.results;
+				} else {
+					data = response.data.results.filter((data) => {
+						return data.name.toLowerCase().match(search.toLowerCase());
+					});
+				}
+				setFilteredData(data);
 			})
 			.catch(function (error) {
 				console.log(error);
 			});
+	}, [search, random]);
 
+	useEffect(() => {
 		axios.get('https://rickandmortyapi.com/api/location')
 			.then(function (response) {
 				setLocations(response.data.results.slice(0, -11));
@@ -40,10 +64,16 @@ export default function Home({ navigation }) {
 
 	return (
 		<SafeAreaView style={tw`bg-emerald-500 h-full `}>
-			<ScrollView stickyHeaderIndices={[0]}>
+			<ScrollView stickyHeaderIndices={[0]} refreshControl={<RefreshControl refreshing={refreshing} onRefresh={onRefresh} />}>
 				<View>
 					<TextInput
+						autoComplete='off'
+						autoCorrect={false}
+						returnKeyType='go'
+						autoCapitalize={false}
 						placeholder='Search by character name'
+						defaultValue={search}
+						onChangeText={(search) => setSearch(search)}
 						style={tw`bg-white py-4 px-5 mx-7 my-6 rounded-xl border border-gray-300 shadow-xl`}
 					/>
 					<View style={tw`absolute top-9 right-10`}>
@@ -56,8 +86,8 @@ export default function Home({ navigation }) {
 				</View>
 
 				<ScrollView horizontal={true} showsHorizontalScrollIndicator={false}>
-					<View style={tw`flex-row justify-center `}>
-						{characters.map((character) => (
+					<View style={tw`flex-row justify-center min-h-70`}>
+						{filteredData.map((character) => (
 							<TouchableOpacity
 								key={character.id}
 								onPress={() => navigation.navigate('CharacterDetails', { character: character })}
